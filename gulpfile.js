142e616efde9bd79a1a5f7f31fa3bd4a66b9eeb2
woods-ming <woodsming@sina.com>
2017-12-12 17:03:20 +0800

    exclude modernizr.js when wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -160,7 +160,7 @@ gulp.task('wiredep', () => {
 
   gulp.src('app/*.html')
     .pipe(wiredep({
-      exclude: ['bootstrap-sass'],
+      exclude: ['bootstrap-sass', 'modernizr'],
       ignorePath: /^(\.\.\/)*\.\./
     }))
     .pipe(gulp.dest('app'));
