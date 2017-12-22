462c28926a57946e12361c972bdf0b82789e6371
woods-ming <woodsming@sina.com>
2017-12-22 21:24:08 +0800

    ajust directory & add truncText

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,20 @@
 $(function() {
+  // 走马灯
   $('.str_wrap').liMarquee();
+
+  // 长文本自动截断
+  /*$(".list-unstyled li a").TruncText({ "singleHeight": "20", "fontSize": "14" });
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    var tabId = $(this).attr("aria-controls");
+    $('#' + tabId).find(".list-unstyled li a").TruncText(20, 14);
+  });
+  $(window).resize(function() {
+    $(".list-unstyled li a").TruncText({ "singleHeight": "20", "fontSize": "14" });
+  });*/
+
+  
+
+  $(window).resize(function() {
+    $("#test").TruncText({ "singleHeight": "20", "fontSize": "14" });
+  });
 });
