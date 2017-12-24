a397a851befb4dd2c19601ee2e4647e29afbe8fc
woods-ming <woodsming@sina.com>
2017-12-24 21:15:52 +0800

    add list.html & detail.html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,13 +3,13 @@ $(function() {
   $('.str_wrap').liMarquee();
 
   //长文本自动截断
-  $('.list-unstyled li a').TruncText({ singleHeight: '26', fontSize: '14' });
+  $('.panel-body li a').TruncText({ singleHeight: '26', fontSize: '14' });
   $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
     var tabId = $(this).attr('aria-controls');
-    $('#' + tabId).find('.list-unstyled li a').TruncText({ singleHeight: '26', fontSize: '14' });
+    $('#' + tabId).find('li a').TruncText({ singleHeight: '26', fontSize: '14' });
   });
   $(window).resize(function() {
-    $('.list-unstyled li a').TruncText({ singleHeight: '26', fontSize: '14' });
+    $('.panel-body li a').TruncText({ singleHeight: '26', fontSize: '14' });
   });
 
 });
